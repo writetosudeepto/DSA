2a63070bfb51d881d7290f1b058e09c428dc0e49
bender <writetosudeepto@gmail.com>
2022-06-28 01:39:59 +0530

    selection sort logic fix

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,8 +1,10 @@
 def insertion_sort(arr):
+    iter_count = 0
     for i in range(1, len(arr)):
         index_to_insert = i
         j = i - 1
         while j >= 0:
+            iter_count += 1
             if arr[j] < arr[index_to_insert]:
                 break
             print("swapping {} and {}".format(arr[j], arr[index_to_insert]))
@@ -10,6 +12,6 @@ def insertion_sort(arr):
             print("array is now {}".format(arr))
             index_to_insert = j
             j -= 1
-    return arr
+    return arr,iter_count
 
-print(insertion_sort([5,4,6,3,2,3,2,1]))
+print(insertion_sort([1,2,3,4,5,6,7,8,9]))
--- sorting/selectionsort.py
+++ sorting/selectionsort.py
@@ -1,5 +1,5 @@
 def selection_sort(arr):
-    for i in range(len(arr)):
+    for i in range(len(arr)-1):
         min_index = i
         for j in range(i+1, len(arr)):
             if arr[j] < arr[min_index]:
