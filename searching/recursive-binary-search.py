29a0b9255b526483209b4985126d90beae608256
bender <writetosudeepto@gmail.com>
2022-07-01 23:32:55 +0530

    recursive binary search variables name change

--- searching/recursive-binary-search.py
+++ searching/recursive-binary-search.py
@@ -1,13 +1,13 @@
 def binary_search(arr,item):
-    def binary_search_recursive(arr,item,low,high,):
-        if low > high:
+    def binary_search_recursive(arr,item,start,end,):
+        if start > end:
             return -1
         else:
-            mid = (low + high) // 2
+            mid = (start+end) // 2
             if arr[mid] == item:
                 return mid
             elif item < arr[mid]:
-                return binary_search_recursive(arr,item,low,mid-1)
+                return binary_search_recursive(arr,item,start,mid-1)
             else:
-                return binary_search_recursive(arr,item,mid+1,high)
+                return binary_search_recursive(arr,item,mid+1,end)
     return binary_search_recursive(arr,item,0,len(arr)-1)
